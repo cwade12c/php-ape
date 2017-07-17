a228c67d436c1d21d397600a23691453d5ebb865
cwade12c <cwade12c@gmail.com>
2017-07-16 19:58:23 -0700

    Reformatted the code style
    Reformatted the code style for the Student section to comply with the Zend standard.

--- Student/getDetailedTable.php
+++ Student/getDetailedTable.php
@@ -1,11 +1,12 @@
 <?php
-	include 'creds.php';
-	$data = array();
-	$idStudent = $_POST["id"];
-	$idExam = $_POST["exam"];
-	
-	try {
-		$stmt = $conn->prepare("SELECT g.grade, c.name AS category, e.possible AS possible
+include 'creds.php';
+$data = array();
+$idStudent = $_POST["id"];
+$idExam = $_POST["exam"];
+
+try {
+    $stmt = $conn->prepare(
+        "SELECT g.grade, c.name AS category, e.possible AS possible
 		FROM category_grades g
 		INNER JOIN assigned_graders a
 		ON a.assigned_exam_id=g.assigned_exam_id
@@ -13,17 +14,17 @@
 		ON e.exam_category_id =a.exam_cat_id
 		INNER JOIN category c
 		ON c.id=e.category_id
-		WHERE g.student_id='".$idStudent."' AND e.exam_id='".$idExam."'"); 
-		$stmt->execute();
-		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-		
-		while($row = $stmt->fetch())
-		{
-			$data[] = $row;
-		}
-		$conn = null;
-		echo json_encode($data);
-	}catch(PDOException $e) {
-		echo "Error: " . $e->getMessage();
-	}
+		WHERE g.student_id='" . $idStudent . "' AND e.exam_id='" . $idExam . "'"
+    );
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    while ($row = $stmt->fetch()) {
+        $data[] = $row;
+    }
+    $conn = null;
+    echo json_encode($data);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 ?>
--- Student/setState.php
+++ Student/setState.php
@@ -1,17 +1,20 @@
 <?php
-	include 'creds.php';
-	$data = array();
-	$idStudent = $_POST["student"];
-	$state = $_POST["theState"];
-	
-	try {
-		$stmt = $conn->prepare("UPDATE student SET state='".$state."' WHERE EWU_ID='".$idStudent."'"); 
-		$stmt->execute();
-		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-		
-		$conn = null;
-		echo json_encode($data);
-	}catch(PDOException $e) {
-		echo "Error: " . $e->getMessage();
-	}
+include 'creds.php';
+$data = array();
+$idStudent = $_POST["student"];
+$state = $_POST["theState"];
+
+try {
+    $stmt = $conn->prepare(
+        "UPDATE student SET state='" . $state . "' WHERE EWU_ID='" . $idStudent
+        . "'"
+    );
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    $conn = null;
+    echo json_encode($data);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 ?>
